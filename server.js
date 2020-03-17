796d4b772e2d75bddd394475053408327ad5a98b
rahaf89 <rahaflh.900@hotmail.com>
2020-03-17 17:36:19 +0100

    product_post

--- server.js
+++ server.js
@@ -82,26 +82,26 @@ app.post("/customers", (req, res) => {
 });
 
 app.post("/products", function(req, res) {
-  const newName = req.body.name;
-  const newPrice = req.body.price;
-  const newSupplierId = req.body.supplierid;
+  const ProductName = req.body.product_name;
+  const ProductPrice = req.body.unit_price;
+  const SupplierId = req.body.supplier_id;
+
+  if (!Number.isInteger(ProductPrice) || ProductPrice <= 0) {
+    return res.status(400).send("The price should be a positive integer.");
+  }
 
   pool
-    .query("SELECT * FROM products WHERE product_name = $1", [newName])
+    .query("SELECT * FROM products WHERE supplier_id=$1", [SupplierId])
     .then(result => {
-      if (result.rows.length > 0) {
-        return res
-          .status(400)
-          .send("A product with the same supplier_id already exists!");
+      if (!SupplierId) {
+        return res.status(400).send("There is no supplier with that id!");
       } else {
         const query =
-          "INSERT INTO products (product_name, unit_price, supplier_id) VALUES ($1, $2, $3)";
-        const params = [newName, newPrice, newSupplierId];
-
+          "INSERT INTO products (product_name, unit_price,supplier_id) VALUES ($1, $2, $3)";
         pool
-          .query(query, params)
-          .then(() => res.send("Customer created"))
-          .catch(e => res.status(500).send(e));
+          .query(query, [ProductName, ProductPrice, SupplierId])
+          .then(() => res.send("Product added!"))
+          .catch(e => console.error(e));
       }
     });
 });
