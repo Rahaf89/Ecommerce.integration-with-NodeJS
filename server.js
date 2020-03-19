055cda42d2a82bbb37ad7942620d6bf136cec8d2
rahaf89 <rahaflh.900@hotmail.com>
2020-03-19 17:10:49 +0100

    delete

--- server.js
+++ server.js
@@ -51,11 +51,19 @@ app.post("/customers", (req, res) => {
     .catch(e => res.status(500).send(e));
 });
 
-app.get("/suppliers", (req, res) => {
-  pool.query("SELECT * FROM suppliers", (error, result) => {
-    res.json(result.rows);
-  });
-});
+app.put("/customers/:customerId", function(req, res) { 
+  const newCustomerName = req.body.name;
+  const newCustomerAddress = req.body.address;
+  const newCustomerCity = req.body.city;
+  const newCustomerCountry = req.body.country;
+  const newcustomerId = req.params.id;
+
+  pool.query(
+    "UPDATE customers SET name=$1, address=$2, city=$3, country=$4 WHERE id=$5", 
+    [newCustomerName, newCustomerAddress,newCustomerCity, newCustomerCountry , newcustomerId])
+      .then(() => res.send(`Customer updated ! `))
+      .catch(e => console.error(e)); 
+}); 
 
 app.get("/products", (req, res) => {
   pool.query(
@@ -72,7 +80,7 @@ app.get("/products", function(req, res) {
   let query = "SELECT * FROM products ORDER BY product_name";
 
   if (productNameQuery) {
-    query = `SELECT * FROM products WHERE product_name is like '%${productNameQuery}%' ORDER BY product_name `;
+    query = `SELECT * FROM products WHERE product_name ilike '%${productNameQuery}%' ORDER BY product_name `;
   }
 
   pool
@@ -104,6 +112,40 @@ app.post("/products", function(req, res) {
           .catch(e => console.error(e));
       }
     });
+});
+
+app.get("/orders/:orderId", function(req, res) {
+  const orderId = req.params.orderId;
+
+  pool
+    .query("SELECT * FROM orders where id = $1", [orderId])
+    .then(result => res.json(result.rows))
+    .catch(err => res.status(500).send(error));
+});
+
+app.get("/customers/:customerId/orders", function(req, res) {
+  const customerId = req.params.customerId; 
+
+  const query = 
+    "select orders.order_date , orders.order_reference , products.product_name, products.unit_price, suppliers.supplier_name , order_items.quantity" + 
+    "from orders join order_items on order_items.order_id =orders.id" + 
+    "join products on order_items.product_id =products.id"+
+    "join suppliers on products.supplier_id = suppliers.id"+
+    "where customerId = $1"; 
+
+  pool
+    .query(query, [customerId])
+    .then(result => res.json(result.rows))
+    .catch(err => res.status(500).send(error)); 
+}); 
+
+
+app.delete("/orders/:orderId", function(req, res) {
+  const orderId = req.params.orderId;
+
+  pool.query("DELETE FROM orders WHERE id=$1", [orderId])
+      .then(() => res.send(`order ${orderId} deleted!`))
+      .catch(e => console.error(e));
 });
 
 app.listen(3000, function() {
